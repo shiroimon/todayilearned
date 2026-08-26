07231cac2b39427bce835fec54d8010e66409e3e
shiroimon <150888300+shiroimon@users.noreply.github.com>
2026-08-26 23:08:19 +0900

    Deploy: 2026-08-26 23:08:19

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ビルド時のタイムスタンプでキャッシュバージョンを自動更新
-const CACHE_VERSION = 'v-1787379237';
+const CACHE_VERSION = 'v-1787753290';
 const CACHE_NAME = `til-blog-${CACHE_VERSION}`;
 
 // キャッシュするリソース（HTMLファイルを除く静的アセットのみ）
